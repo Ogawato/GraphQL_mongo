8f33308238577e7a8da514d4ecdf53e751f5893b
Ogawato <t-ogawa@yairilab.net>
2021-10-23 11:01:53 +0900

    レイアウト作成

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Container, Col, Row } from 'reactstrap';
+import Header from './components/Header';
+import MovieList from './components/MovieList';
+import SideNav from './components/SideNav';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Container>
+        <Row>
+          <Col xs={12} sm={4}>
+            <SideNav />
+          </Col>
+          <Col xs={12} sm={8}>
+            <MovieList />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
